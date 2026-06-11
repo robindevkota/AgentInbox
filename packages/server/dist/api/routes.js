d0196910371de1eda10d71d94ccd8718b4604507
Robin Devkota <robin.devkota@amniltech.com>
2026-06-11 22:44:01 +0545

    fix: send feedback emails to feedback@useagentinbox.com

--- packages/server/dist/api/routes.js
+++ packages/server/dist/api/routes.js
@@ -121,7 +121,7 @@ function createRouter() {
       </div>`;
         try {
             const mailer = await Promise.resolve().then(() => __importStar(require("../email/mailer")));
-            await mailer.send("robin@useagentinbox.com", subject, html);
+            await mailer.send("feedback@useagentinbox.com", subject, html);
         }
         catch { }
         res.json({ ok: true });
